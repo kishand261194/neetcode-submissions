4e114021fd8fb43f58c8612bd798c937cdc572dc
Kishan Dhamotharan <kishand.261194@gmail.com>
2026-07-25 10:55:07 -0700

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleanS = ""
+        
+        for c in s:
+            if c.isalnum():
+                cleanS += c.lower()
+
+        reverse = cleanS[::-1]
+        print(cleanS, reverse)
+        return reverse == cleanS
+
