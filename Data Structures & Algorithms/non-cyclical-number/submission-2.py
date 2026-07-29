feb5a2cd844d8250f9446bf7667d39c654e92619
Kishan Dhamotharan <kishand.261194@gmail.com>
2026-07-28 17:14:05 -0700

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        nstr = str(n)
+        seen = set()
+        ssum = 0
+        while (ssum !=1): 
+            print(ssum,seen,nstr)
+            ssum = 0  
+            for i in nstr:
+                ssum += int(i) ** 2
+            if (ssum in seen):
+                break          
+            seen.add(ssum)
+            nstr = str(ssum)
+            print(ssum,seen,nstr)
+
+        return ssum == 1
